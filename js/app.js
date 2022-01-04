e48900ae852fd6ff3654e838716237caa57b88e2
hasan-naim <hasannaim6246@gmail.com>
2022-01-04 15:46:00 +0600

    added functionality to add new todo

--- js/app.js
+++ js/app.js
@@ -2,83 +2,65 @@
 const inputElement = document.querySelector(".header input");
 const headerDiv = document.querySelector(".header");
 
-
 const addBtn = document.querySelector(".addBtn");
 const listUl = document.querySelector(".list");
-
-
-inputElement.addEventListener("focus",() => {
-    headerDiv.classList.add("big-width")
-
-} )
+let id = 0;
+inputElement.addEventListener("focus", () => {
+    headerDiv.classList.add("big-width");
+});
 inputElement.addEventListener("blur", () => {
-    headerDiv.classList.remove("big-width")
-})
-addBtn.addEventListener("click", ()=> {
-    if(inputElement.value === ""){
-        inputElement.focus()
-    }
- 
-})
-addBtn.addEventListener("mouseover", ()=> {
-    inputElement.blur()
-    headerDiv.classList.add("big-width")
-})
+    headerDiv.classList.remove("big-width");
+});
 
-
-
-
-
-
-
-
-
-
+addBtn.addEventListener("mouseover", () => {
+    inputElement.blur();
+        headerDiv.classList.add("big-width");
+    
+});
 
 /// adding todo
 
 
+document.addEventListener("keypress", (event) => {
 
+            inputElement.focus();
+        
+});
 
-
-
-
-
-
-
-
-
-document.addEventListener("keypress", (event)=> {
+// press enter key
+document.addEventListener("keyup", (event)=> {
     if(event.key ==="Enter"){
-        if(!inputElement.value === ""){
-
-            addTodo()
-        }else{
-            inputElement.focus()
+        if (inputElement.value === "") {
+            inputElement.focus();
+            return;
+        }else {
+            const text = inputElement.value
+            addTodo(text, id)
+            id++;
         }
-    }else{
-
-        inputElement.focus()
     }
 })
 
+// what will happen when user click on add button
+addBtn.addEventListener("click", () => {
+    if (inputElement.value === "") {
+        inputElement.focus();
+        return;
+    }else {
+        const text = inputElement.value
+        addTodo(text, id)
+        id++;
+    }
+});
 
+function addTodo(text, id) {
+    const html = `
+    <li class="item" id=${id}>
+                    <input type="checkbox">
+                    <p>${text}</p>
+                    <i class="bi bi-trash-fill"></i>
+                </li>
+    `;
 
-
-
-
-
-
-
-
-
-// document.addEventListener("keyup", (event)=> {
-//     if(event.key ==="Enter"){
-//         addTodo()
-//     }
-// })
-addBtn.addEventListener("click", addTodo)
-
-function addTodo(text){
-    console.log("hello")
+    listUl.insertAdjacentHTML("afterbegin", html);
 }
